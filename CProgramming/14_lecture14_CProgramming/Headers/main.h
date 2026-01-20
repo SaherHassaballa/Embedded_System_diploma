0dec6f528fc4c26079a129b084a3e59436a71a18
Saher Hassaballa <saherayman290@gmail.com>
2026-01-20 22:52:34 +0200

    lec14

---
+++ CProgramming/14_lecture14_CProgramming/Headers/main.h
@@ -0,0 +1,6 @@
+struct student
+{
+    unsigned char SName[30];
+    float Degree ; 
+    int ID ;
+};
---
+++ CProgramming/14_lecture14_CProgramming/Sources/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#include "C:\Users\saher\Desktop\Github Projects\Embedded_System_diploma\CProgramming\14_lecture14_CProgramming\Headers\main.h"
+
+int main(){
+    struct Std1 ;
+}
