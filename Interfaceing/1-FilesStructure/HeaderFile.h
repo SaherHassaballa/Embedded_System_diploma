72a2cadf66f2e6064c55e02bab2f0fb178a20f9f
Saher Hassaballa <saherayman290@gmail.com>
2026-06-27 02:43:19 +0300

    interface 2_GPIO_APPLICATION add Folders for layers to project

--- Interfaceing/1-FilesStructure/HeaderFile.h
+++ Interfaceing/1-FilesStructure/HeaderFile.h
@@ -32,3 +32,55 @@ Std_ReturnType MSSP_I2C_Init(const mssp_i2c_init *i2c_obj);
 
 
 #endif
+
+
+
+/********************************************************************* */
+
+/*
+ * File     : HeaderFile.h
+ * Author   : Saher Hassaballa
+ * LinkedIn : https://www.linkedin.com/in/saher-hassaballah-36a922196/
+ * Created  : May 21, 2025, 9:53 pm
+ * Brief    : Header file for module declarations
+ */
+
+#ifndef HeaderFile_H
+#define HeaderFile_H
+
+/*--------------------------- Includes --------------------------*/
+#include <stdint.h>
+#include <stdbool.h>
+
+/*-------------------- Macro Declarations ----------------------*/
+#define PI                      3.14
+#define MODULE_MAX_COUNT       4U
+#define MODULE_ENABLE          1U
+#define MODULE_DISABLE         0U
+
+/*---------------- Macro Function Declarations -----------------*/
+#define I2C_SLEW_RATE_DISABLE_CFG()     (SSPSTATbits.SMP = 1)
+#define IS_VALID_ID(id)                 ((id) < MODULE_MAX_COUNT)
+
+/*-------------------- Data Type Declarations ------------------*/
+typedef enum
+{
+    E_OK = 0,
+    E_NOT_OK
+} Std_ReturnType;
+
+typedef struct
+{
+    uint8_t port;
+    uint8_t pin;
+} module_config_t;
+
+/*---------------- External Variables Declarations ------------*/
+/* extern uint8_t global_counter; */
+
+/*---------------- Software Interfaces Declarations -----------*/
+Std_ReturnType MODULE_Init(const module_config_t *config);
+Std_ReturnType MODULE_Start(void);
+Std_ReturnType MODULE_Stop(void);
+
+#endif
--- Interfaceing/1-FilesStructure/SourceFile.c
+++ Interfaceing/1-FilesStructure/SourceFile.c
@@ -15,3 +15,89 @@
 
 
 /*------------------------Inline function Definition if not above-----------------------*/
+/************************************************************************************************** */
+
+/*
+ * File     : SourceFile.c
+ * Author   : Saher Hassaballa
+ * LinkedIn : https://www.linkedin.com/in/saher-hassaballah-36a922196/
+ * Created  : May 21, 2025, 9:53 pm
+ * Brief    : Source file for module implementation
+ */
+
+/*--------------------------- Includes --------------------------*/
+#include "HeaderFile.h"
+
+/*-------------------- Local Macro Declarations ----------------*/
+#define HW_PORT_BASE_ADDR      (0x40020000UL)
+
+/*-------------- Local Macro Function Declarations -------------*/
+#define SET_BIT(REG,BIT)       ((REG) |= (1U << (BIT)))
+#define CLEAR_BIT(REG,BIT)     ((REG) &= ~(1U << (BIT)))
+
+/*---------------- Local Data Type Declarations ----------------*/
+typedef struct
+{
+    bool initialized;
+} module_runtime_t;
+
+/*------------------------ Global Variables --------------------*/
+/* uint8_t global_counter = 0; */
+
+/*------------------------ Local Variables ---------------------*/
+static module_runtime_t module_state = {0};
+
+/*---------------- Local Function Prototypes -------------------*/
+static void Module_HardwareInit(void);
+static void Module_Reset(void);
+
+/*---------------- Software Interfaces Definitions -------------*/
+Std_ReturnType MODULE_Init(const module_config_t *config)
+{
+    Std_ReturnType ret = E_NOT_OK;
+
+    if (config != NULL)
+    {
+        Module_HardwareInit();
+        module_state.initialized = true;
+        ret = E_OK;
+    }
+
+    return ret;
+}
+
+Std_ReturnType MODULE_Start(void)
+{
+    Std_ReturnType ret = E_NOT_OK;
+
+    if (module_state.initialized)
+    {
+        ret = E_OK;
+    }
+
+    return ret;
+}
+
+Std_ReturnType MODULE_Stop(void)
+{
+    Std_ReturnType ret = E_NOT_OK;
+
+    if (module_state.initialized)
+    {
+        Module_Reset();
+        ret = E_OK;
+    }
+
+    return ret;
+}
+
+/*-------------------- Local Function Definitions --------------*/
+static void Module_HardwareInit(void)
+{
+    /* Hardware initialization */
+}
+
+static void Module_Reset(void)
+{
+    /* Reset hardware */
+}
