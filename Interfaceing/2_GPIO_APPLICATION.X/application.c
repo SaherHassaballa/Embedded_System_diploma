7a025581d0b65f8f2adbb439667b05ee5115b911
Saher Hassaballa <saherayman290@gmail.com>
2026-06-29 01:26:04 +0300

    add gpio code in application layer this not true

--- Interfaceing/2_GPIO_APPLICATION.X/application.c
+++ Interfaceing/2_GPIO_APPLICATION.X/application.c
@@ -6,12 +6,26 @@
  */
 
 
+ /*this not true due to if change mc family architure will change in application so should written in mcal layer*/
 #include "application.h"
-/*
- * 
- */
-int main(int argc, char** argv) {
+
+#define HWREG8(_x)        (*((volatile uint8 *) (_x)))
+
+#define AHMED_LATC        (HWREG8(0xF8B))
+#define AHMED_TRISC       (HWREG8(0xF94))
+
+int main()
+{
+    AHMED_TRISC = 0x00;
+
+    while(1)
+    {
+        AHMED_LATC = 0x0F;
+        __delay_ms(1000);
+
+        AHMED_LATC = 0xF0;
+        __delay_ms(1000);
+    }
 
     return (EXIT_SUCCESS);
 }
-
